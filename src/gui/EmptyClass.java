0c5df92c2a0ecbeec43e833d588a870eb74a4995
natha <nathanael.mercier2007@gmail.com>
2025-12-19 17:17:14 +0100

    empty class creation to see the package

---
+++ src/gui/EmptyClass.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class EmptyClass {
+	/*
+	 * classe présente pour faire apparaitre 
+	 * les packages qu'on doit respecter, une
+	 * fois que vous commencez à coder dans un
+	 * package vous pourrez supprimer
+	 * cette classe
+	 */
+}
---
+++ src/metier/EmptyClass.java
@@ -0,0 +1,11 @@
+package metier;
+
+public class EmptyClass {
+	/*
+	 * classe présente pour faire apparaitre 
+	 * les packages qu'on doit respecter, une
+	 * fois que vous commencez à coder dans un
+	 * package vous pourrez supprimer
+	 * cette classe
+	 */
+}

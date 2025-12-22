f19508c072ee4258959f92b7b14d9e77a2387d6a
natha <nathanael.mercier2007@gmail.com>
2025-12-22 15:28:35 +0100

    bibliotheque.sql completed

--- database/bibliotheque.sql
+++ database/bibliotheque.sql
@@ -3,15 +3,135 @@ CREATE TABLE Client (
 	nom VARCHAR(80) NOT NULL,
 	prenom VARCHAR(30) NOT NULL,
 	abonnementId INT NOT NULL,
-	compteId INT NOT NULL
+	compteId INT NOT NULL,
+	CONSTRAINT pkClientId PRIMARY KEY (clientId),
+	CONSTRAINT fkAbonnementId1 FOREIGN KEY (abonnementId)
+	REFERENCES Abonnement(abonnementId),
+	CONSTRAINT fkCompteId1 FOREIGN KEY (compteId)
+	REFERENCES Compte(compteId)
 )
 
 CREATE TABLE Abonnement (
 	abonnementId INT AUTO_INCREMENT NOT NULL,
 	type VARCHAR(20),
-	prix DECIMAL(4,2) NOT NULL
+	prix DECIMAL(4,2) NOT NULL,
+	CONSTRAINT pkAbonnementId PRIMARY KEY(abonnementId)
 )
 
-CREATE TABLE Session(
-	//je dois finir le fichier sql
+CREATE TABLE Compte(
+	compteId INT AUTO_INCREMENT NOT NULL,
+	identifiant VARCHAR(30) NOT NULL,
+	mdpHash VARCHAR(100) NOT NULL,
+	dateCreation DATE NOT NULL,
+	type VARCHAR(15) NOT NULL, --bibliothecaire ou client
+	CONSTRAINT pkCompteId PRIMARY KEY (compteId)
 )
+
+CREATE TABLE Date(
+	dateId INT AUTO_INCREMENT NOT NULL,
+	dateDuJour DATE DEFAULT CURDATE(),
+	dateManuelle BOOLEAN DEFAULT FALSE, --permet de changer la date manuellement si true
+	CONSTRAINT pkDateId PRIMARY KEY (dateId)
+)
+
+CREATE TABLE Bibliothecaire(
+	bibliothecaireId INT AUTO_INCREMENT NOT NULL,
+	nom VARCHAR(80) NOT NULL,
+	prenom VARCHAR(30) NOT NULL,
+	compteId INT NOT NULL,
+	CONSTRAINT pkBibliothecaireId PRIMARY KEY (bibliothecaireId),
+	CONSTRAINT fkCompteId2 FOREIGN KEY(compteId)
+	REFERENCES Compte(compteId)
+)
+
+CREATE TABLE Emprunt(
+	objetId INT AUTO_INCREMENT NOT NULL,
+	dateDebut DATE NOT NULL,
+	dateFin DATE,
+	dureeMaximaleEmprunt DECIMAL(8,4), --durée en jours, si on devait mettre des durées de moins d'1 jour, il faudrait choisir des heures multiples de 3 (3h = 0,125 jour)
+	clientId INT NOT NULL,
+	CONSTRAINT pkObjetId PRIMARY KEY (objetId),
+	CONSTRAINT fkObjetId1 FOREIGN KEY (objetId)
+	REFERENCES Objet(objetId),
+	CONSTRAINT fkClientId1 FOREIGN KEY (clientId)
+	REFERENCES Client(clientId)
+)
+
+CREATE TABLE Objet(
+	objetId INT AUTO_INCREMENT NOT NULL,
+	nom VARCHAR(100) NOT NULL, --titre pour un livre, modele pour un ordinateur, titre d'un CD...
+	auteur VARCHAR(100), --auteur du livre, du DVD, constructeur de l'ordinateur
+	prix DECIMAL(4,2),
+	type VARCHAR(10) NOT NULL, --JeuSociete, Ordinateur, Livre, CD, DVD
+	quantite INT NOT NULL DEFAULT 1,
+	reference VARCHAR(100) NOT NULL, --Pour un livre ISBN, pour les autres code barre ou qqch comme ça qui identifie dans le cas où on aurait deux objets de même nom
+	CONSTRAINT pkObjetId PRIMARY KEY (objetID)
+)
+
+CREATE TABLE JeuSociete(
+	jeuSocieteId INT AUTO_INCREMENT NOT NULL,
+	nom VARCHAR(100) NOT NULL,
+	auteur VARCHAR(100),
+	dateAchat DATE NOT NULL,
+	reference VARCHAR(100) NOT NULL,
+	objetId INT NOT NULL,
+	CONSTRAINT fkObjetId2 FOREIGN KEY (objetId)
+	REFERENCES Objet(objetId)
+)
+
+CREATE TABLE CD(
+	cdId INT AUTO_INCREMENT NOT NULL,
+	titre VARCHAR(100) NOT NULL,
+	auteur VARCHAR(100),
+	dateAchat DATE NOT NULL,
+	reference VARCHAR(100) NOT NULL,
+	genre VARCHAR(50), --Rock, Pop, Mathématiques, espagnol...
+	objetId INT NOT NULL,
+	CONSTRAINT fkObjetId3 FOREIGN KEY (objetId)
+	REFERENCES Objet(objetId)
+)
+
+CREATE TABLE Livre(
+	livreId INT AUTO_INCREMENT NOT NULL,
+	titre VARCHAR(100) NOT NULL,
+	auteur VARCHAR(100),
+	dateAchat DATE NOT NULL,
+	referenceISBN VARCHAR(100) NOT NULL,
+	genre VARCHAR(50), --Roman historique, société, Policier, magazine, BD, manga...
+	objetId INT NOT NULL,
+	CONSTRAINT fkObjetId4 FOREIGN KEY (objetId)
+	REFERENCES Objet(objetId)
+)
+
+CREATE TABLE DVD(
+	dvdId INT AUTO_INCREMENT NOT NULL,
+	titre VARCHAR(100) NOT NULL,
+	auteur VARCHAR(100),
+	dateAchat DATE NOT NULL,
+	reference VARCHAR(100) NOT NULL,
+	genre VARCHAR(50), -- Thriller, comédie, géopolitique...
+	objetId INT NOT NULL,
+	CONSTRAINT fkObjetId5 FOREIGN KEY (objetId)
+	REFERENCES Objet(objetId)
+)
+
+CREATE TABLE ORDINATEUR(
+	dvdId INT AUTO_INCREMENT NOT NULL,
+	titre VARCHAR(100) NOT NULL,
+	auteur VARCHAR(100),
+	dateAchat DATE NOT NULL,
+	reference VARCHAR(100) NOT NULL,
+	genre VARCHAR(50), -- Thriller, comédie, géopolitique...
+	objetId INT NOT NULL,
+	CONSTRAINT fkObjetId5 FOREIGN KEY (objetId)
+	REFERENCES Objet(objetId)
+)
+
+--On fait pas de table catégorie finalement--
+
+-- a chaque lancement de l'application pour que la date se mette à jour
+--UPDATE system_date
+--SET current_date = CURRENT_DATE
+--WHERE id = 1;
+
+--pouvoir changer la date manuellement pour pouvoir tester--

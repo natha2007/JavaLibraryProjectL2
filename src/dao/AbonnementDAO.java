aa827624ffd338ad2d75f22a5ddc946b0b527d00
natha <nathanael.mercier2007@gmail.com>
2025-12-23 16:00:11 +0100

    derniers changements sur AbonnementDAO + création de ClientDAO

---
+++ src/dao/AbonnementDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.sql.ResultSet;
+
+import metier.Abonnement;
+
+public class AbonnementDAO extends DAO<Abonnement>{
+	private ResultSet rs;
+
+	@Override
+	public Abonnement create(Abonnement obj) {
+		return null;
+	}
+
+	@Override
+	public Abonnement update(Abonnement obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Abonnement obj) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void read(String id) {
+		
+	}
+
+}
---
+++ src/dao/ClientDAO.java
@@ -0,0 +1,29 @@
+package dao;
+
+import metier.Client;
+
+public class ClientDAO extends DAO<Client>{
+
+	@Override
+	public Client create(Client obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Client update(Client obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Client obj) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void read(String id) {
+		
+	}
+
+}

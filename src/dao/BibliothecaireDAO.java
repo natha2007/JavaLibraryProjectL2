bd17b75336451d95659ed081dd2f4df734e14c7d
natha <nathanael.mercier2007@gmail.com>
2026-01-08 11:48:25 +0100

    Bibliothecaire + BibliothecaireDAO finis
    Il reste plus qu'à faire la classe de test

---
+++ src/dao/BibliothecaireDAO.java
@@ -0,0 +1,81 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import metier.Bibliothecaire;
+import metier.Compte;
+
+public class BibliothecaireDAO extends DAO<Bibliothecaire> {
+	private ResultSet rs;
+
+	@Override
+	public Bibliothecaire create(Bibliothecaire bbl) {
+		String requete = "INSERT INTO bibliothecaire(nom, prenom, compteId)"
+				+ " VALUES ('" + bbl.getNom() + "', '" + bbl.getPrenom() + "', " + 
+				bbl.getCompte().getCompteId() + ")";
+		try {
+			stmt.executeUpdate(requete, Statement.RETURN_GENERATED_KEYS);
+			rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				bbl.setBibliothecaireId(rs.getInt(1));
+			}
+		} catch (SQLException e) {
+			System.out.println("erreur requête SQL");
+			e.printStackTrace();
+		}
+		return bbl;
+	}
+
+	@Override
+	public Bibliothecaire update(Bibliothecaire bbl) {
+		String requete = "UPDATE bibliothecaire"
+				+ " SET nom= '" + bbl.getNom()
+				+ "', prenom= '" + bbl.getPrenom()
+				+ "', compte= " + bbl.getCompte().getCompteId()
+				+ " WHERE bibliothecaireId= " + bbl.getBibliothecaireId();
+		try {
+			stmt.executeUpdate(requete);
+		} catch (SQLException e) {
+			System.out.println("erreur requête SQL");
+			e.printStackTrace();
+		}
+		return bbl;
+	}
+
+	@Override
+	public void delete(Bibliothecaire bbl) {
+		String requete = "DELETE FROM bibliothecaire"
+				+ " WHERE bibliothecaireId=" + bbl.getBibliothecaireId();
+		try {
+			stmt.executeUpdate(requete);
+		} catch (SQLException e) {
+			System.out.println("erreur requête SQL");
+			e.printStackTrace();
+		}	
+	}
+	
+	public Bibliothecaire read(Integer id) {
+		Bibliothecaire bbl = null;
+		Compte c = null;
+		CompteDAO cd = new CompteDAO();
+		String requete = "SELECT *"
+				+ " FROM bibliothecaire"
+				+ " WHERE bibliothecaireId=" + id;
+		try {
+			this.rs = stmt.executeQuery(requete);
+			if (rs.next()) {
+				c = cd.read(rs.getInt(4));
+				bbl = new Bibliothecaire(rs.getInt(1), rs.getString(2), rs.getString(3), c);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("erreur requête SQL");
+		}
+				
+		
+		return bbl;
+	}
+	
+}

7b8f0a21ee3a7ce1bc56d409cb52aef9ff1762d5
natha <nathanael.mercier2007@gmail.com>
2026-01-10 22:04:01 +0100

    corresctions de bug + code plus propre
    Pour BibliothequePage et PageClient

--- src/gui/BibliothecairePage.java
+++ src/gui/BibliothecairePage.java
@@ -5,62 +5,90 @@ import java.awt.*;
 import javax.swing.*;
 
 public class BibliothecairePage extends JPanel {
+	
+	private Runnable rb;
 	private CardLayout cl;
 	private CompteUtilisateur user;
+	private JPanel head;
+	private JPanel body;
+	private JPanel foot;
+	private JLabel mainText;
+	private JButton abonnements;
+	private JButton emprunts;
+	private JButton commandes;
+	private JButton stocks;
+	private JButton retours;
+	private JLabel footText;
+	private JButton accueil;
 	
 	public void setUser(CompteUtilisateur user) {
 		this.user = user;
-		JPanel head = new JPanel(new GridLayout(0,5,0,0)); //int rows, int cols, int hgap, int vgap
+		majUI();
+		
+	}
+	
+	private void initialiserUI() {
+		setLayout(new BorderLayout());
+		head = new JPanel(new GridLayout(0,5,0,0)); //int rows, int cols, int hgap, int vgap
 		head.setPreferredSize(new Dimension(0,50));
 		
-		JPanel body = new JPanel(new GridBagLayout());
-		JPanel foot = new JPanel(new GridLayout(0,2,20,0));
+		body = new JPanel(new GridBagLayout());
+		foot = new JPanel(new GridLayout(0,2,20,0));
 		
 		add(head, BorderLayout.NORTH);
 		add(body, BorderLayout.CENTER);
 		add(foot, BorderLayout.SOUTH);
 		
-		JLabel text = new JLabel("salut je suis le logiciel bibliothèque de merde");
-		body.add(text);
+		mainText = new JLabel();
+		body.add(mainText);
 		
-		JButton abonnements = new JButton("Abonnements");
-		JButton emprunts = new JButton("Emprunts");
-		JButton commandes = new JButton("Commandes");
-		JButton stocks = new JButton("Stocks");
-		JButton retours = new JButton("Retours");
+		abonnements = new JButton("Abonnements");
+		emprunts = new JButton("Emprunts");
+		commandes = new JButton("Commandes");
+		stocks = new JButton("Stocks");
+		retours = new JButton("Retours");
 
-		
 		head.add(abonnements);
 		head.add(emprunts);
 		head.add(commandes);
 		head.add(stocks);
 		head.add(retours);
 		
-		JLabel footText = new JLabel("Gestionnaire bibliothèque - 2026 Tous droits réservés");
-		JButton accueil = new JButton("Accueil");
+		footText = new JLabel("Gestionnaire bibliothèque - 2026 Tous droits réservés");
+		accueil = new JButton("Accueil");
 		
 		foot.add(footText);
 		foot.add(accueil);
+		
+		setMaximumSize(new Dimension(100,100));
+	}
+	
+	private void majUI() {
+		if (user == null) {
+			mainText.setText("En attente de connexion");
+		} else {
+			mainText.setText("salut je suis le logiciel bibliothèque");
+			//user n'est pas utilisé pour l'instant mais pourra l'être
+		}
+		//accueil.addActionListener(e->conn.run());
+		//abonnements.addActionListener(e->conn.run());
 	}
 	
 	public BibliothecairePage(Runnable rb) {
-		
-		setLayout(new BorderLayout());
-		
-		
-		
-		//accueil.addActionListener(e->conn.run());
-		
-		//abonnements.addActionListener(e->conn.run());
+		this.rb = rb;
+		initialiserUI();
+		majUI();
 	}
 	
 	//Méthode de test
 	/*
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame("Test PageClient");
+		JFrame frame = new JFrame("Test PageBibliothecaire");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(new MenuLogiciel());
+        frame.setContentPane(new BibliothecairePage(() -> {
+            System.out.println("Switch de page ignoré");
+        }));
         frame.setMinimumSize(new Dimension(900,600));
         frame.setVisible(true);
         
--- src/gui/PageClient.java
+++ src/gui/PageClient.java
@@ -13,67 +13,98 @@ import metier.*;
 
 public class PageClient extends JPanel {
 	
+	private Runnable rb;
 	private CompteUtilisateur user;
+	private JLabel profil;
+	private JLabel profilInfos;
+	private JPanel body;
+	private JTable table;
+	private JScrollPane scroll;
 	
 	public void setUser(CompteUtilisateur user) {
 		this.user = user;
+		majUI();
+	}
+	
+	/**
+	 * Initialiser les éléments statiques de l'ui.
+	 * Donc soit créer les composants qui ne vont pas bouger
+	 * soit juste initialiser les éléments qui accueilleront 
+	 * du contenu différent selon l'user.
+	 */
+	private void initialiserUI() {
+		setLayout(new BorderLayout());
 		
+		profil = new JLabel("Profil");
+		add(profil, BorderLayout.NORTH);
+		
+		profilInfos = new JLabel();
+		add(profilInfos, BorderLayout.NORTH);
+		
+		body = new JPanel(new BorderLayout());
+		add(body, BorderLayout.CENTER);
+		
+//		body = new JPanel(new GridLayout(10,1));
+//		add(body, BorderLayout.CENTER);
+	}
+	
+	/**
+	 * définit ce qui peut être mis à jour,
+	 * c'est à dire ce qui dépend de l'utilisateur
+	 * (user)
+	 */
+	private void majUI() {
 		LocalDate today = LocalDate.now();
 		ClientDAO cd = new ClientDAO();
 		EmpruntDAO ed = new EmpruntDAO();
-		Client cl = cd.read(user.getClientId());
-		ArrayList<Emprunt> listeEmprunt = ed.getListeEmpruntsByClientId(user.getClientId());
-		
-		JLabel test2 = new JLabel("clientId : " + user.getClientId() + ", prenom : " + cl.getPrenom() + ", nom : " + cl.getNom());
-		add(test2, BorderLayout.NORTH);
-		
-		JPanel body = new JPanel(new BorderLayout());
-		add(body, BorderLayout.CENTER);
-//		JPanel body = new JPanel(new GridLayout(10,1));
-//		add(body, BorderLayout.CENTER);
-		
-		String[] colonnes = {
-		        "N° de l'emprunt",
-		        "Date de l'empreint",
-		        "Date d'expiration",
-		        "Jours restants"
-		};
-		DefaultTableModel tabRes = new DefaultTableModel(colonnes, 0);
-
-		for (Emprunt e : listeEmprunt) {
-			LocalDate localDateFin = LocalDate.parse(e.getDateFin());
-
-			Object[] ligne = {
-				e.getEmpruntId(),
-				e.getDateDebut(),
-				e.getDateFin(),
-				ChronoUnit.DAYS.between(today, localDateFin)
+		if (user == null) {
+			profilInfos.setText("attente de connexion");
+		} else {
+			Client cl = cd.read(user.getClientId());
+			ArrayList<Emprunt> listeEmprunt = ed.getListeEmpruntsByClientId(user.getClientId());
+			profilInfos.setText("clientId : " + user.getClientId() + ", prenom : " + cl.getPrenom() + ", nom : " + cl.getNom());
+			
+			String[] colonnes = {
+			        "N° de l'emprunt",
+			        "Date de l'empreint",
+			        "Date d'expiration",
+			        "Jours restants"
 			};
-
-			tabRes.addRow(ligne);
-//			LocalDate localDateFin = LocalDate.parse(e.getDateFin());
-//			JLabel test3 = new JLabel("Numéro emprunt : " + e.getEmpruntId()
-//									+ ", Date de l'emprunt : " + e.getDateDebut()
-//									+ ", Date d'expiration :" + e.getDateFin()
-//									+ ", Nombre de jours restants : " + ChronoUnit.DAYS.between(today, localDateFin)
-//
-//									);
-//			body.add(test3);
+			DefaultTableModel tabRes = new DefaultTableModel(colonnes, 0);
+	
+			for (Emprunt e : listeEmprunt) {
+				LocalDate localDateFin = LocalDate.parse(e.getDateFin());
+	
+				Object[] ligne = {
+					e.getEmpruntId(),
+					e.getDateDebut(),
+					e.getDateFin(),
+					ChronoUnit.DAYS.between(today, localDateFin)
+				};
+	
+				tabRes.addRow(ligne);
+	//			LocalDate localDateFin = LocalDate.parse(e.getDateFin());
+	//			JLabel test3 = new JLabel("Numéro emprunt : " + e.getEmpruntId()
+	//									+ ", Date de l'emprunt : " + e.getDateDebut()
+	//									+ ", Date d'expiration :" + e.getDateFin()
+	//									+ ", Nombre de jours restants : " + ChronoUnit.DAYS.between(today, localDateFin)
+	//
+	//									);
+	//			body.add(test3);
+			}
+			
+			table = new JTable(tabRes);
+			table.setRowHeight(25);
+			table.setFont(new Font("Arial", Font.PLAIN, 14));
+			scroll = new JScrollPane(table);
+			body.setLayout(new BorderLayout());
+			body.add(scroll, BorderLayout.CENTER);
 		}
-		JTable table = new JTable(tabRes);
-		table.setRowHeight(25);
-		table.setFont(new Font("Arial", Font.PLAIN, 14));
-
-		JScrollPane scroll = new JScrollPane(table);
-		body.setLayout(new BorderLayout());
-		body.add(scroll, BorderLayout.CENTER);
-
 	}
 	
 	public PageClient(Runnable rb) {
-		setLayout(new BorderLayout());
-		JLabel test = new JLabel("Profil");
-		add(test, BorderLayout.NORTH);
-	
+		this.rb = rb;
+		initialiserUI();
+		majUI();
 	}
 }
